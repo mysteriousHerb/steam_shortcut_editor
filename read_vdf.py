dba014a63b59266f8d9770312d0884e627dfba7d
Tianheng <herbzhao@users.noreply.github.com>
2022-09-13 09:26:39 +0100

    using griddb and steamlibrary to find games info

--- read_vdf.py
+++ read_vdf.py
@@ -5,9 +5,13 @@ import requests
 import os
 import time
 
+import requests
+from bs4 import BeautifulSoup
+import os, sys
+
 applist_path = "steam_applist.json"
 
-# load steam library
+
 
 # check created time, if older than 1 day, update
 if os.path.exists(applist_path):
@@ -20,19 +24,16 @@ else:
     r = requests.get(url, allow_redirects=True)
     open('steam_applist.json', 'wb+').write(r.content)
 
-with open('applist.json') as f:
-    steam_applist = json.load(f)
+with open('applist.json', encoding="UTF-8") as f:
+    steam_applist = json.load(f,)
     steam_applist = steam_applist["applist"]["apps"]
 
 # convert lists of dictionary into two lists for fast searching
 applist_names = [i["name"] for i in steam_applist]
 applist_appids = [i["appid"] for i in steam_applist]
 
-def search_steam_applist(game_name):
-    url = f"https://www.steamgriddb.com/api/v2/search/autocomplete/{game_name}"
-    headers = {"Authorization": "Bearer 446cecd21f68a3385bcdfe953ccc7390"}
-    r = requests.get(url, headers=headers)
-    return r.json()
+
+
 
 sgdb = SteamGridDB('446cecd21f68a3385bcdfe953ccc7390')
 # load all the non-steam game shortcuts
@@ -41,19 +42,30 @@ with open('shortcuts.vdf', 'rb') as appinfo:
 
 for shortcut in shortcuts.values():
     # using griddb to find nearest match
-    match_results = sgdb.search_game(shortcut["AppName"])
-    # using the first result as closest match - future can be selectable by user
-    if len(match_results) > 0:
-        game_name = match_results[0].name
-        # find index of game by matching the name
-        try: 
-            index = applist_names.index(game_name)
-            # get the id of the game
-            appid = applist_appids[index]
-            print(f"Game: {game_name}, ID: {appid}")
+    if shortcut["AppName"].isnumeric():
+        appid = int(shortcut["AppName"])
+        try:
+            index = applist_appids.index(appid)
+            game_name = applist_names[index]
+            print(f"ID: {appid}: Game: {game_name}")
         except ValueError:
-            print(f"Game: {game_name}: not found")
+            print(f"ID: {appid}: not found")
 
-    else:
-        print(f"Game: {game_name}: not found")
+    else: 
+        match_results = sgdb.search_game(shortcut["AppName"])
+        if len(match_results) == 0:
+            print(f'Game: {shortcut["AppName"]}: not found in steamgriddb')
+        # using the first result as closest match - future can be selectable by user
+        else:
+            game_name = match_results[0].name
+            # find index of game by matching the name
+            try: 
+                index = applist_names.index(game_name)
+                # get the id of the game
+                appid = applist_appids[index]
+                print(f"Game: {game_name}, ID: {appid}")
+            except ValueError:
+                print(f"Game: {game_name}: not found")
 
+    # else:
+    #     print(f"Game: {game_name}: not found")
---
+++ test.py
@@ -0,0 +1,10 @@
+import vdf
+from steamgrid import SteamGridDB
+import json
+import requests
+import os
+import time
+
+match_results = sgdb.search_game("witcher3")
+print(match_results)
+
