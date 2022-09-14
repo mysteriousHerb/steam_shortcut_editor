fd20547d402a47b1435fe250c4fccae690a9d2b4
HERB Z <herbzhao@gmail.com>
2022-09-14 10:19:17 +0100

    removed the need of applist.json

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
-import vdf
-shortcut_path = "shortcuts.vdf"
-with open(shortcut_path, 'rb') as appinfo:
-    shortcuts = vdf.binary_loads(appinfo.read())["shortcuts"]
-    shortcut_names = [i["AppName"] for i in shortcuts.values()]
+import requests
+from bs4 import BeautifulSoup
 
-print(shortcuts["1"])
+
+appid = "690040"
+url = f"https://store.steampowered.com/app/{appid}"
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0"}
+soup = BeautifulSoup(requests.get(url, headers=headers).text, 'html.parser')
+
+# find class of img_ctn
+img_ctn = soup.find(class_=["game_header_image_ctn", "img_ctn"])  
+
+if not img_ctn:
+    print("Not found")
+else:
+    # find image url
+    game_name = soup.find("title").text.replace(" on Steam", "")
+    image_url = img_ctn.find("img")["src"]
+    print(game_name)
+    print(image_url)
